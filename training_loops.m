b9bb6b5c31f26f3d8479711744f748f68013a683
Jorisbottelier <jorisbottelier2@gmail.com>
2023-01-08 16:02:21 +0100

    added a loop for training different sparsities

--- training_loops.m
+++ training_loops.m
@@ -11,12 +11,18 @@
 % Creates a list of 10 values for G between 1 and 10 and trains the network
 % for each value
 
-Gs = 1:10;
-parfor i=1:length(Gs)
-    filename = ['simtest3_' num2str(i)];
-    run_sim(2000, 200, 600, 100, 1, 1, Gs(i), 1,...
-    1, 0.05, 0, true, true, 'spikes', ['.' filesep 'Output' filesep filename]);
-end
+% Gs = 1:10;
+% parfor i=1:length(Gs)
+%     filename = ['simtest3_' num2str(i)];
+%     run_sim(2000, 200, 600, 100, 1, 1, Gs(i), 1,...
+%     1, 0.05, 0, true, true, 'spikes', ['.' filesep 'Output' filesep filename]);
+% end
 
 % Looping over the sparsity:
 
+sparsities = 0:0.1:1;
+parfor i=1:length(sparsities)
+    filename = ['sparsity_simtest' num2str(i)];
+    sparsity_run_sim(2000, 200, 600, 100, 1, 1, 4, 1,...
+    1, 0.05, 0, sparsities(i), true, true, 'spikes', ['.' filesep 'Output' filesep filename]);
+end
